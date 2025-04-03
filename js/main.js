792887b7d581baf9521b220642ef7ca14f0b31bd
lgazzola <lgazzola87@gmail.com>
2025-04-03 19:54:44 +0200

    refactored without phaser

--- js/main.js
+++
@@ -1,7 +0,0 @@
-import { setupMenu } from './menu.js';
-import { initializeGame } from './game.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  setupMenu();
-  initializeGame();
-});
